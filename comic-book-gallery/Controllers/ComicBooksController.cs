e6f428bd187bb066e5121183b566af59c5ad8719
Vrijesh Patel <60836824+vrijeshpatel1997@users.noreply.github.com>
2021-02-20 04:52:49 -0700

    added viewbag

--- comic-book-gallery/Controllers/ComicBooksController.cs
+++ comic-book-gallery/Controllers/ComicBooksController.cs
@@ -12,6 +12,21 @@ namespace comic_book_gallery.Controllers
         public ActionResult Detail()
         {
 
+
+
+            ViewBag.SeriesTitle = "The amazibg spiderman";
+            ViewBag.issueNumber = 700;
+            ViewBag.description = "<p> Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+
+            ViewBag.artists = new string[]
+          {
+        "Script: Dan Slott",
+        "Pencils: Humberto Ramos",
+        "Inks: Victor Olazaba",
+        "Colors: Edgar Delgado",
+        "Letters: Chris Eliopoulos"
+          };
+
             return View();
 
 
