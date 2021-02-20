16c576ac65e28307dbf28d68cd334c0aa6e434a9
Vrijesh Patel <60836824+vrijeshpatel1997@users.noreply.github.com>
2021-02-20 09:55:48 -0700

    added repository

--- comic-book-gallery/Controllers/ComicBooksController.cs
+++ comic-book-gallery/Controllers/ComicBooksController.cs
@@ -14,30 +14,14 @@ namespace comic_book_gallery.Controllers
         public ActionResult Detail()
         {
 
-            ComicBook comic = new ComicBook() {
-                SeriesTitle = "The amazibg spiderman",
-                IssueNumber = 700,
-                DescriptionHTML = "<p> Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-
-                    new Artist () { Name = "Dan Slott", Role = " Script"},
-                      new Artist () { Name = "Humberto Ramos", Role = "Pencils"},
-                        new Artist () { Name = "Victor Olazaba", Role = "Inks"},
-                          new Artist () { Name = "Edgar Delgado", Role = "Colors"},
-                            new Artist () { Name = "Chris Eliopoulos", Role = "Letters"},
-                }
-
-
-
-            };
+          
 
 
           
 
      
 
-            return View(comic);
+            return View();
 
 
         }
